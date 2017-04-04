b271b83491a4f36b986bf7ea239343e2ad38feaf
= <=>
2017-04-04 13:11:40 +0530

    Sec commit

--- Watches/src/main/java/com/watchesfront/controller/HomeController.java
+++ Watches/src/main/java/com/watchesfront/controller/HomeController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-
+	//To display Home Page
 	@RequestMapping("/Home")
 	public String showHome()
 	{
